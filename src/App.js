38e0e16f10c1018731d96ea180e1c4fa6ecab324
Cynthia <cynthiaphan98@gmail.com>
2022-10-25 18:51:03 -0400

    Add profile-cropped.JPG Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Info from './components/Info.js'
+import About from './components/About'
+import Interests from './components/Interests'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
     </div>
   );
 }
