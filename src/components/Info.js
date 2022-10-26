9687944d96b4c541d1d0a157430218a24e49a7c1
Cynthia <cynthiaphan98@gmail.com>
2022-10-25 22:17:52 -0400

    Edit and add style to Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,12 +3,20 @@ import img from "../profile-cropped.JPG"
 
 function Info() {
     return (
-        <div>
-            <img src={img} alt="Cynthia Phan's profile" />
-            <h1>Cynthia Phan</h1>
-            <p>First-year Student @ UdeM</p>
-            <button></button>
-            <button></button>
+        <div className="info">
+            <img src={img} alt="Cynthia Phan's profile" width="100%"/>
+            <h1 className="info--name">Cynthia Phan</h1>
+            <p className="info--student">First-year Student @ UdeM</p>
+            <div className="info--buttons">
+                <button className="info--email">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3296/3296467.png" alt="Email" width="16px"/>
+                    <p>Email</p>
+                </button>
+                <button className="info--linkedin">
+                    <img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/linkedin-icon-18-256.png" alt="LinkedIn" width="16px"/>
+                    <p>LinkedIn</p>
+                </button>
+            </div>
         </div>
     )
 }
