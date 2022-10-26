9e93fbddc24c02e538b1bf7609c0e4194f41f9e3
Cynthia <cynthiaphan98@gmail.com>
2022-10-26 00:04:23 -0400

    Edit Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,14 +8,18 @@ function Info() {
             <h1 className="info--name">Cynthia Phan</h1>
             <p className="info--description">First-year Student in BSc Computer Science @ UdeM</p>
             <div className="info--buttons">
-                <button className="info--email">
-                    <img src="https://cdn-icons-png.flaticon.com/512/3296/3296467.png" alt="Email" width="16px"/>
-                    <p>Email</p>
-                </button>
-                <button className="info--linkedin">
-                    <img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/linkedin-icon-18-256.png" alt="LinkedIn" width="16px"/>
-                    <p>LinkedIn</p>
-                </button>
+                <a  href="mailto:cynthiaphan.code@gmail.com">
+                    <button className="info--email">
+                        <img src="https://cdn-icons-png.flaticon.com/512/3296/3296467.png" alt="Email" width="16px"/>
+                        <p>Email</p>
+                    </button>
+                </a>
+                <a  href="https://www.linkedin.com/in/cynthia-phan-a94308232/" target="_blank">
+                    <button className="info--linkedin">
+                        <img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/linkedin-icon-18-256.png" alt="LinkedIn" width="16px"/>
+                        <p>LinkedIn</p>
+                    </button>
+                </a>
             </div>
         </div>
     );
