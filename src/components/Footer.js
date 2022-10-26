3205e9e129ff6b08caf7028a6a155b4aef151c44
Cynthia <cynthiaphan98@gmail.com>
2022-10-26 00:10:32 -0400

    Edit Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,18 @@ import React from "react";
 function Footer() {
     return (
         <div className="footer">
-            <img src="https://cdn-icons-png.flaticon.com/512/3291/3291695.png" alt="GitHub" width="25px"/>
-            <img src="https://cdn-icons-png.flaticon.com/512/3536/3536423.png" alt="Facebook" width="25px"/>
-            <img src="https://cdn-icons-png.flaticon.com/512/3536/3536608.png" alt="Pinterest" width="25px"/>
-            <img src="https://cdn-icons-png.flaticon.com/512/1419/1419647.png" alt="Instagram" width="25px"/>
+            <a href="https://github.com/cphan98" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/3291/3291695.png" alt="GitHub" width="25px"/>
+            </a>
+            <a href="https://www.facebook.com/phttc98/" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/3536/3536423.png" alt="Facebook" width="25px"/>
+            </a>
+            <a href="https://www.pinterest.ca/cynthiaphan98/" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/3536/3536608.png" alt="Pinterest" width="25px"/>
+            </a>
+            <a href="https://www.instagram.com/cphan98/" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/1419/1419647.png" alt="Instagram" width="25px"/>
+            </a>
         </div>
     );
 }
